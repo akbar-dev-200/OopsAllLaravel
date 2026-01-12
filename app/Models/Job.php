a6b879b855bee7f2811960953bae5d3c37a4e531
Muhammad Akbar <muhammadakbar@ITs-MacBook-Pro.local>
2026-01-12 16:23:01 +0500

    gif added

---
+++ app/Models/Job.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    protected $table = 'job_listings';
+
+    protected $fillable = [
+        'name',
+        'salary',
+
+    ];
+}
